42840fb9ac87673abca464fae0648b4072eb78fe
Eduaerdo2023 <147427368+Eduaerdo2023@users.noreply.github.com>
2024-09-22 22:38:07 -0600

    Capitulo 3 minuto 1:05

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,34 +1,113 @@
 import asyncHandler from "../middlewares/asyncHandler.js";
 import User from "../models/userModal.js";
 import bcrypt from "bcryptjs";
+import createToken from "../utils/createToken.js";
 
 const createUser = asyncHandler(async (req, res) => {
   const { username, email, password, isAdmin } = req.body;
   if (!username || !email || !password) {
     throw new Error("Please fill all the inputs");
   }
-
   const userExists = await User.findOne({ email });
   if (userExists) return res.status(400).send("Email already exists");
-
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
-
-  const newUser = new User({ username, email, password: hashedPassword});
+  const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
-    res
-      .status(201)
-      .json({
-        _id: newUser._id,
-        username: newUser.username,
-        email: newUser.email,
-        isAdmin: newUser.isAdmin,
-      });
+    createToken(res, newUser._id);
+    res.status(201).json({
+      _id: newUser._id,
+      username: newUser.username,
+      email: newUser.email,
+      isAdmin: newUser.isAdmin,
+    });
   } catch (error) {
     res.status(400);
     throw new Error("Invalid user data");
   }
 });
 
-export { createUser };
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+    if (isPasswordValid) {
+      createToken(res, existingUser._id);
+      res.status(200).json({
+        _id: existingUser._id,
+        username: existingUser.username,
+        email: existingUser.email,
+        isAdmin: existingUser.isAdmin,
+      });
+      return;
+    } else {
+       res.status(400);
+       throw new Error("Invalid user data");
+    }
+  }
+});
+
+const logoutCurrentUser = asyncHandler(async (req, res) => {
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: "Logout successfully" });
+});
+
+const getAllUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+});
+
+const getCurrentUser = asyncHandler(async (req, res) => {
+  const currentUser = await User.findById(req.user._id);
+  if (currentUser) {
+    res.json({
+      _id: currentUser._id,
+      username: currentUser.username,
+      email: currentUser.email,
+    });
+  }
+});
+
+const updateProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    user.username = req.body.username || user.username;
+    if (req.body.password) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(req.body.password, salt);
+      user.password = hashedPassword;
+    }
+    const updatedUser = await user.save();
+    res.json({
+      _id: updatedUser._id,
+      username: updatedUser.username,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+const deleteUser = asyncHandler(async(req, res)=> {
+const user = await User.findById(req.params.id);
+console.log(user);
+
+})
+
+export {
+  createUser,
+  loginUser,
+  logoutCurrentUser,
+  getAllUsers,
+  getCurrentUser,
+  updateProfile,
+  deleteUser
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,20 @@
 import express from "express";
-const router = express.Router()
-import { createUser } from "../controllers/userControllers.js";
+const router = express.Router();
+import {
+  createUser,
+  deleteUser,
+  getAllUsers,
+  getCurrentUser,
+  loginUser,
+  logoutCurrentUser,
+  updateProfile,
+} from "../controllers/userControllers.js";
+import { authenticate, authorizedAdmin } from "../middlewares/authMiddleware.js";
 
-router.route('/').post(createUser)
 
-export default router
+router.route("/").post(createUser).get(authenticate, authorizedAdmin, getAllUsers)
+router.post("/auth", loginUser);
+router.get("/logout", logoutCurrentUser);
+router.route('/profile').get(authenticate, getCurrentUser).put(authenticate,updateProfile)
+router.route("/:id").delete(authenticate, authorizedAdmin, deleteUser)
+export default router;
